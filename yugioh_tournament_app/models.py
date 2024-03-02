2a3932f4e731ba427179588ba013d80200b958f5
hivancd <111187629+hivancd@users.noreply.github.com>
2024-03-02 11:38:45 -0500

    player model complete

--- yugioh_tournament_app/models.py
+++ yugioh_tournament_app/models.py
@@ -6,41 +6,101 @@ from phonenumber_field.formfields import PhoneNumberField
 from phonenumber_field.phonenumber import PhoneNumber
 from django import forms
 import datetime as dt
-# Create your models here.
+from django.contrib.auth.models import User
+
+    
+def copy_fields(original,copy):
+    keys = list(original.__dict__.keys())[2:]
+    for k in keys:
+        copy.__setattr__(k,original.__dict__[k])
 
 class Player(models.Model):
             
-    first_name=models.CharField(max_length=200)
-    last_name=models.CharField(max_length=200)
+    # EN EL MODELO USER VIENEN PROPS DE USERNAME, FIRST_NAME, LAST_NAME, IS_STAFF, PASSWORD, ETC
+    # Esas nos interesan
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
     second_last_name=models.CharField(max_length=200)
     province=models.CharField(max_length=200)
     municipality=models.CharField(max_length=200)
     phone=models.CharField(max_length=15,blank=True)
     address=models.CharField(max_length=200)
     
-    def insert_new_player(first_name,last_name,second_last_name,province,municipality,phone,address):
+    def insert(username,password,first_name,last_name,second_last_name,province,municipality,phone,address,is_staff=False):
+        if(username[0]=='@'):
+            raise ValidationError('Username cant start with the character @')
         Player.is_valid_phonenumber(phone)
-        p = Player(first_name=first_name,last_name=last_name,second_last_name=second_last_name,province=province,municipality=municipality,phone=phone,address=address)
+        u=User(username=username,password=password,first_name=first_name,last_name=last_name,is_staff=is_staff)
+        p = Player(user=u,second_last_name=second_last_name,province=province,municipality=municipality,phone=phone,address=address)
+        u.save()
         p.save()
-    
-    def add_deck(self,deck_name,main_deck,side_deck,extra_deck,archtype='Mixto'):
-        Deck.insert_deck(deck_name,main_deck,side_deck,extra_deck,self,archtype)
+        return p
         
+    def get_fake_user(self):
+        fake_user=User()
+        user_keys=list(self.user.__dict__.keys())[2:]
+        for uk in user_keys:
+            fake_user.__setattr__(uk,self.user.__dict__[uk])
+        fake_user.username = '@' + fake_user.username
+        return fake_user
+        
+    def get_copy(self):
+        copy = Player() 
+        keys = list(self.__dict__.keys())[2:]
+        for k in keys:
+            copy.__setattr__(k,self.__dict__[k])
+        return copy
+
+        
+    def modify(self, attributes:list, values:list):
+        prev:Player = self.get_copy()
+        fake_user:User = self.get_fake_user()
+        for i in range(len(attributes)):
+            try:
+                prev.__getattribute__(attributes[i])
+                prev.__setattr__(attributes[i], values[i])
+            except:
+                # print('not player props')
+                try:
+                    fake_user.__getattribute__(attributes[i])
+                    val=values[i]
+                    if(attributes[i]=='username'):
+                        if val[0]=='@':
+                            raise ValidationError('Username cant start with @')
+                        val='@'+val
+                    fake_user.__setattr__(attributes[i], val)
+                except:
+                    print('property not found.')
+                    pass    
+        try:
+            Player.is_valid_phonenumber(prev.phone)
+            prev.clean_fields(exclude= ['user'])
+            fake_user.full_clean()
+            copy_fields(prev,self)
+            fake_user.username = fake_user.username[1:]
+            copy_fields(fake_user,self.user)
+        except:
+            print('validation error')
+        return
+          
     def is_valid_phonenumber(phone):
         if phone:
             try:
                 if not PhoneNumber.from_string(phone).is_valid():
-                    raise ValidationError('The phone number field is not correct.')
+                    raise ValueError('The phone number field is not correct.')
             except:
-                raise ValidationError('The phone number field is not correct.')
+                raise ValueError('The phone number field is not correct.')
     
+    def add_deck(self,deck_name,main_deck,side_deck,extra_deck,archtype='Mixto'):
+        Deck.insert_deck(deck_name,main_deck,side_deck,extra_deck,self,archtype)
+        
     def fullname(self):
-        return ' '.join([self.first_name,self.last_name,self.second_last_name.strip()])
+        return self.user.username
     
     def __str__(self):
         return self.fullname()    
     
 class Deck(models.Model): 
+    
     MIN_MAIN_DECK_SIZE=40
     AMMOUNT_OF_CARDS_PER_DECK_TYPE={
         'main_deck':60,
@@ -66,13 +126,22 @@ class Tournament(models.Model):
     tournament_name=models.CharField(max_length=200)
     start_datetime=models.DateTimeField()
     address=models.CharField(max_length=200)
+    champion=models.OneToOneField(Player,on_delete=models.RESTRICT,blank=True,default=None)
     
     def __str__(self) -> str:
         return self.tournament_name
     
-    def insert_tournament(name,start_datetime,address):
-        t=Tournament(tournament_name=name,start_datetime=start_datetime,address=address)
+    def insert_tournament(name,start_datetime,address,champion=None):
+        t=Tournament(tournament_name=name,start_datetime=start_datetime,address=address,champion=champion)
         t.save()
+    
+    def check_champion_is_participant(self,champion):
+        participants= self.participants.all()
+        print(participants)
+        
+    def set_champion(self,player):
+        self.check_champion_is_participant(player)
+        
     
 class TournamentParticipant(models.Model): # TEST PENDING
     tournament=models.ManyToManyField(Tournament, related_name='participants')
@@ -144,3 +213,15 @@ class Duel(models.Model):
         
     def __str__(self):
         return f'The player {self.player1} faced {self.player2} in the tournament: "{self.tournament}" in the {self.tournament_phase}.'
+    
+# from yugioh_tournament_app.models import *
+# Player.insert_new_player('ale','jan','dro','Matanzas','Varadero','','otracasa')
+# Player.insert_new_player('mau','ri','cio','sancti spiritu','majayigua','','sucasa')
+# p1=Player.objects.get(pk=1)
+# p2=Player.objects.get(pk=2)
+# p1.add_deck('deck de alejandro',40,10,10)
+# p2.add_deck('deck de mauricio',45,13,2)
+# d1=Deck.objects.get(pk=1)
+# from datetime import datetime as dt
+# from datetime import timedelta as delta
+# Tournament.insert_tournament('torneo de alejandro y mauricio',dt.now(),'el rinconcito del amor')
